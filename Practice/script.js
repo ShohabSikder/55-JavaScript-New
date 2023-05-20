d8d9b6150905669dd3bb04ab7bc25c821ecb7cb8
ShohabSikder <132428937+ShohabSikder@users.noreply.github.com>
2023-05-20 13:01:34 +0600

    Operator

---
+++ Practice/script.js
@@ -0,0 +1,21 @@
+// let x=10;
+// let y=50;
+// let result=(x+y);
+// // console.log(result);
+
+// let radious=10;
+// const pi=3.2545;
+// let areaOfCircle=pi*radious*radious;
+// // console.log(areaOfCircle);
+
+// logical operator < > == != <= >= and(&&) or (||) not (!)
+ let x=10;
+ let y=5;
+
+console.log(x>y);
+console.log(x<y);
+console.log(x==y);
+console.log(x<=y);
+console.log(x>=y);
+console.log(x=y);
+console.log(x!=y);
