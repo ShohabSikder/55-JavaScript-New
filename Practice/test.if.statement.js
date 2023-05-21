2e39347d0227974e520c101954e47d40ab80679a
ShohabSikder <132428937+ShohabSikder@users.noreply.github.com>
2023-05-21 13:06:00 +0600

    Class-16
    If Conditional

---
+++ Practice/test.if.statement.js
@@ -0,0 +1,72 @@
+// let x=16;
+
+// if(x%3==1){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+
+// }
+
+
+
+// if(weather<0){
+//     console.log("Invalid Weather")
+
+// }
+// else if( weather<=18){
+//     console.log("Cool Weather")
+// }
+// else if( weather<=27){
+//     console.log("Normal Weather")
+// }
+// else{
+//     console.log("abnormal Weather")
+// }
+
+// let weather=18;
+// if(weather>28){
+//     console.log("Abnormal Weather")
+
+//     }
+//     else if(weather>19){
+//         console.log("Normal Weather")
+//     }
+//     else if(weather>0){
+//         console.log("cool Weather")
+//     }
+//     else{
+//         console.log("Invalid Weather")
+//     }
+//  for(let i=7; i<=100; i++){
+//     if(i%2==1)
+//     {console.log(i);
+//     }
+//     else{
+       
+//     }
+    
+//  }
+
+// for(let i=1; i<=50; i++){
+//     if(i%3==0 && i%5==0){
+//         console.log(i);
+
+//     }
+
+// else{
+// }
+
+// }
+
+// 5
+// 5*4
+// 5*4*3
+// 5*4*3*2
+// 5*4*3*2*1
+let input=10;
+let result=2;
+for(let i=input; i>0; i--){
+     result=result*i;
+}
+console.log(result);
