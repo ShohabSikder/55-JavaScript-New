4bbc1b0bf4bddd20a31c247c55f97d69b8f9ba6d
ShohabSikder <132428937+ShohabSikder@users.noreply.github.com>
2023-05-22 13:06:31 +0600

    Class-17
    Loop

---
+++ Practice/test.loop.js
@@ -0,0 +1,133 @@
+// let userName="Java"
+// let password="asd123"
+
+// if(userName=="Java"){
+// if(password=="asd123"){
+//     console.log("Welcome"+ userName)
+
+// }
+// else{
+//     console.log("password is incorrect")
+// }
+
+// }
+// else{
+// console.log("username is not found")
+// }
+
+// Result Sheet
+// let number = -55;
+// if (number >= 101) {
+//     console.log("Invalid Number")
+// }
+// else if (number >= 80) {
+//     console.log("A+")
+// }
+// else if
+//     (number >= 79) {
+//     console.log("A")
+// }
+// else if
+//     (number >= 69) {
+//     console.log("A-")
+// }
+// else if
+//     (number >= 69) {
+//     console.log("A")
+// }
+// else if
+//     (number >= 59) {
+//     console.log("B")
+// }
+// else if
+//     (number >= 49) {
+//     console.log("C")
+// }
+// else if
+//     (number >= 39) {
+//     console.log("D")
+// }
+
+// else if (number >= 32) {
+//     console.log("F")
+
+// }
+
+// else {
+//     console.log("Invalid Number")
+// }
+
+
+
+// Switch Case
+// let day=5
+// switch(day){
+//         case 1:
+//         console.log('Saturday')
+//         break;
+//         case 2:
+//         console.log('Sunday')
+//         break;  
+//         case 3:
+//         console.log('Monday')
+//         break;
+//         case 4:
+//         console.log('Tuesday')
+//         break;
+//         default:
+//      console.log('Nothing found')
+
+// }
+
+
+// Prime Number
+// let counter=0;
+// let number=10;
+// for(let i=1; i<=number; i++){
+//     if(number%i==0){
+//         counter=counter+1;
+//     }
+// }
+// if(counter==2){
+//     console.log('prime number');
+// }
+// else{
+//     console.log('Not prime number');
+// }
+
+// Swap
+// let x=10;
+// let y=20;
+// let temp=0;
+// temp=x;
+// x=y;
+// y=temp;
+
+// console.log('x= '+x+' y= '+y);
+
+let n1=0;
+let n2=1;
+let sum=0;
+
+
+// console.log(n1 +"\n" +n2)
+
+// sum=n1+n2;
+// console.log(sum);
+
+
+console.log(n1);
+console.log(n2);
+
+
+for(let i=3; i<=8; i++){
+    sum=n1+n2
+    console.log(sum);
+    n1=n2;
+    n2=sum;
+}
+
+
+
+
+
